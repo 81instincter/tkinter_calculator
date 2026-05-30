b0fd8819d450f86e7aa5f85ecc0f8965d0feeb25
Elijah Kenning <elijah.kenning@gmail.com>
2026-05-30 11:08:33 -0500

    Add files via upload
    Finalized the color scheme and updated the backspace_icon.png image.

--- customtkinter_calculator.py
+++ customtkinter_calculator.py
@@ -17,7 +17,7 @@ expr = ""
 past_expressions_list = []
 past_results_list = []
 
-img = Image.open("backspace_icon.png")
+img = Image.open("delete.png")
 
 def press(key):
     global expr 
@@ -104,191 +104,192 @@ if __name__ == "__main__":
 
     # Displaying the numbers pressed and their results on the top display bar.
     display = StringVar()
-    entry = CTkEntry(master=root, textvariable=display, fg_color="#220A2E", border_color="#F3F3FF", width=500, height=100)
+    entry = CTkEntry(master=root, textvariable=display, 
+                     fg_color="#BAA7BF", border_color="#F3F3FF", text_color="#514354",
+                     width=500, height=100, font=("Arial", 30))
     entry.grid(columnspan=4, ipadx=70)
 
     # Displaying past arithmetic expressions and their results.
     calc_history_box = CTkTextbox(master=root, 
-                         fg_color="#153755", #bg_color="#770c41",
-                         scrollbar_button_color="#52017b",
-                         width=300, height=100, corner_radius=16)
+                         border_color="#EAE2ED",fg_color="#D7C9DB", text_color="#514354", scrollbar_button_color="#724060",
+                         width=300, height=100, border_width = 0.5, corner_radius=16)
     calc_history_box.grid(row=0, column=4, padx = 10, pady=10)
 
     # =================================================
     # Number buttons: define number button widgets, and set their locations.
     zero_button = CTkButton(master=root, text="0", 
-                            fg_color = "#02203c", bg_color="#02203c", text_color="#6b4082",
+                            border_color = "papaya whip", fg_color = "#2A0C2F", bg_color="papaya whip", hover_color = "#2B3A67", text_color="white",
                             command=lambda: press(0),
-                            height=75, width=75, font=("Arial",28))
+                            height=75, width=75, border_width = 0.5, font=("Arial",28))
     zero_button.grid(row=7, column=1, padx=0, pady=0, sticky="nsew")
     
     one_button = CTkButton(master=root, text="1",
-                           fg_color="black", bg_color="papaya whip",
+                           border_color = "papaya whip", fg_color="#2A0C2F", bg_color="papaya whip", hover_color = "#2B3A67", text_color="white",
                            command=lambda: press(1),
-                           height=75, width=75, font=("Arial",28))
+                           height=75, width=75, border_width = 0.5, font=("Arial",28))
     one_button.grid(row=4, column=0, padx=0, pady=0, sticky="nsew")
 
     two_button = CTkButton(master=root, text="2",
-                           fg_color="black", bg_color="papaya whip",
+                           border_color = "papaya whip", fg_color="#2A0C2F", bg_color="papaya whip", hover_color = "#2B3A67", text_color="white",
                            command=lambda: press(2),
-                           height=75, width=75, font=("Arial",28))
+                           height=75, width=75, border_width = 0.5, font=("Arial",28))
     two_button.grid(row=4, column=1, padx=0, pady=0, sticky="nsew")
 
     three_button = CTkButton(master=root, text="3", 
-                             fg_color="black", bg_color="papaya whip",
+                             border_color = "papaya whip", fg_color="#2A0C2F", bg_color="papaya whip", hover_color = "#2B3A67", text_color="white",
                              command=lambda: press(3),
-                             height=75, width=75, font=("Arial",28))
+                             height=75, width=75, border_width = 0.5, font=("Arial",28))
     three_button.grid(row=4, column=2, padx=0, pady=0, sticky="nsew")
 
     four_button = CTkButton(master=root, text="4",
-                            fg_color="black", bg_color="papaya whip",
+                            border_color = "papaya whip", fg_color="#2A0C2F", bg_color="papaya whip", hover_color = "#2B3A67", text_color="white",
                             command=lambda: press(4),
-                            height=75, width=75, font=("Arial",28))
+                            height=75, width=75, border_width = 0.5, font=("Arial",28))
     four_button.grid(row=5, column=0, padx=0, pady=0, sticky="nsew")
 
     five_button = CTkButton(master=root, text="5",
-                            fg_color="black", bg_color="papaya whip",
+                            border_color = "papaya whip", fg_color="#2A0C2F", bg_color="papaya whip", hover_color = "#2B3A67", text_color="white",
                             command=lambda: press(5),
-                            height=75, width=75, font=("Arial",28))
+                            height=75, width=75, border_width = 0.5, font=("Arial",28))
     five_button.grid(row=5, column=1, padx=0, pady=0, sticky="nsew")
 
     six_button = CTkButton(master=root, text="6",
-                           fg_color="black", bg_color="papaya whip",
+                           border_color = "papaya whip", fg_color="#2A0C2F", bg_color="papaya whip", hover_color = "#2B3A67", text_color="white",
                            command=lambda: press(6),
-                           height=75, width=75, font=("Arial",28))
+                           height=75, width=75, border_width = 0.5, font=("Arial",28))
     six_button.grid(row=5, column=2, padx=0, pady=0, sticky="nsew")
 
     seven_button = CTkButton(master=root, text="7",
-                             fg_color="black", bg_color="papaya whip",
+                             border_color = "papaya whip", fg_color="#2A0C2F", bg_color="papaya whip", hover_color = "#2B3A67", text_color="white",
                              command=lambda: press(7),
-                             height=75, width=75, font=("Arial",28))
+                             height=75, width=75, border_width = 0.5, font=("Arial",28))
     seven_button.grid(row=6, column=0, padx=0, pady=0, sticky="nsew")
 
     eight_button = CTkButton(master=root, text="8",
-                             fg_color="black", bg_color="papaya whip",
+                             border_color = "papaya whip", fg_color="#2A0C2F", bg_color="papaya whip", hover_color = "#2B3A67", text_color="white",
                              command=lambda: press(8),
-                             height=75, width=75, font=("Arial",28))
+                             height=75, width=75, border_width = 0.5, font=("Arial",28))
     eight_button.grid(row=6, column=1, padx=0, pady=0, sticky="nsew")
 
     nine_button = CTkButton(master=root, text="9",
-                            fg_color="black", bg_color="papaya whip",
+                            border_color = "papaya whip", fg_color="#2A0C2F", bg_color="papaya whip", hover_color = "#2B3A67", text_color="white",
                             command=lambda: press(9),
-                            height=75, width=75, font=("Arial",28))
+                            height=75, width=75, border_width = 0.5, font=("Arial",28))
     nine_button.grid(row=6, column=2, padx=0, pady=0, sticky="nsew")
 
     # ==================================
     # Operator Buttons.
     div_button = CTkButton(master=root, text='/',
-                            fg_color="black", bg_color="papaya whip",
+                            border_color = "papaya whip", fg_color="#2A0C2F", bg_color="papaya whip", hover_color = "#2B3A67", text_color="white",
                             command=lambda: press('/'),
-                            height=75, width=75, font=("Arial",28))
+                            height=75, width=75, border_width = 0.5, font=("Arial",28))
     div_button.grid(row=2, column=3, padx=0, pady=0, sticky="nsew")
 
     mult_button = CTkButton(master=root, text='*',
-                            fg_color="black", bg_color="papaya whip",
+                            border_color = "papaya whip", fg_color="#2A0C2F", bg_color="papaya whip", hover_color = "#2B3A67", text_color="white",
                             command=lambda: press('*'),
-                            height=75, width=75, font=("Arial",28))
+                            height=75, width=75, border_width = 0.5, font=("Arial",28))
     mult_button.grid(row=3, column=3, padx=0, pady=0, sticky="nsew")
 
     minus_button = CTkButton(master=root, text='-',
-                            fg_color="black", bg_color="papaya whip",
+                            border_color = "papaya whip", fg_color="#2A0C2F", bg_color="papaya whip", hover_color = "#2B3A67", text_color="white",
                             command=lambda: press('-'),
-                            height=75, width=75, font=("Arial",28))
+                            height=75, width=75, border_width = 0.5, font=("Arial",28))
     minus_button.grid(row=4, column=3, padx=0, pady=0, sticky="nsew")
     
     plus_button = CTkButton(master=root, text='+',
-                            fg_color="black", bg_color="papaya whip",
+                            border_color = "papaya whip", fg_color="#2A0C2F", bg_color="papaya whip", hover_color = "#2B3A67", text_color="white",
                             command=lambda: press('+'),
-                            height=75, width=75, font=("Arial",28))
+                            height=75, width=75, border_width = 0.5, font=("Arial",28))
     plus_button.grid(row=5, column=3, padx=0, pady=0, sticky="nsew")
 
     # ==================================
     # Other Buttons.
 
     modulo_button = CTkButton(master=root, text='%',
-                            fg_color="black", bg_color="papaya whip",
+                            border_color = "papaya whip", fg_color="#2A0C2F", bg_color="papaya whip", hover_color = "#2B3A67", text_color="white",
                             command=lambda: press('%'),
-                            height=75, width=75, font=("Arial",28))
+                            height=75, width=75, border_width = 0.5, font=("Arial",28))
     modulo_button.grid(row=1, column=0, padx=0, pady=0, sticky="nsew")
 
     clear_entry_button = CTkButton(master=root, text='CE',
-                            fg_color="black", bg_color="papaya whip",
+                            border_color = "papaya whip", fg_color="#2A0C2F", bg_color="papaya whip", hover_color = "#2B3A67", text_color="white",
                             command=clear_entry,
-                            height=75, width=75, font=("Arial",28))
+                            height=75, width=75, border_width = 0.5, font=("Arial",28))
     clear_entry_button.grid(row=1, column=1, padx=0, pady=0, sticky="nsew")
 
     clear_button = CTkButton(master=root, text="Clear",
-                            fg_color="black", bg_color="papaya whip",
+                            border_color = "papaya whip", fg_color="#2A0C2F", bg_color="papaya whip", hover_color = "#2B3A67", text_color="white",
                             command=clear,
-                            height=75, width=75, font=("Arial",28))
+                            height=75, width=75, border_width = 0.5, font=("Arial",28))
     clear_button.grid(row=1, column=2, padx=0, pady=0, sticky="nsew")
 
-    clear_history_button = CTkButton(master=root, text="Clear History",
-                                     fg_color="black", 
+    clear_history_button = CTkButton(master=root, text="Clear History", 
+                                     border_color = "papaya whip", hover_color = "#2B3A67", text_color="white", fg_color="#2A0C2F", 
                                      command=clear_history,
-                                     height=30, width=50)
+                                     height=30, width=50, border_width = 0.5, font=("Arial",20))
     clear_history_button.grid(row=1, column=4, padx=10, pady=10, sticky="nsew")
 
 # image is there but is just black and blends in with button
     backspace_button = CTkButton(master=root, text="",
-                            fg_color="black", bg_color="papaya whip",
+                            border_color = "papaya whip", fg_color="#2A0C2F", bg_color="papaya whip", hover_color = "#2B3A67", text_color="white",
                             command=backspace,
-                            height=75, width=75, font=("Arial",28),
+                            height=75, width=75, border_width = 0.5, font=("Arial",28),
                             image = CTkImage(dark_image=img, light_image=img))
     backspace_button.grid(row=1, column=3, padx=0, pady=0, sticky="nsew")
 
     inverse_button = CTkButton(master=root, text="1/x",
-                               fg_color="black", bg_color="papaya whip",
+                               border_color = "papaya whip", fg_color="#2A0C2F", bg_color="papaya whip", hover_color = "#2B3A67", text_color="white",
                                command=inverse, 
-                               height=75, width=75, font=("Arial",28))
+                               height=75, width=75, border_width = 0.5, font=("Arial",28))
     inverse_button.grid(row=2, column=0, padx=0, pady=0, sticky="nsew") # padx=0 and pady=0 removes spacing between widget and its grid neighbors
 
     exp_button = CTkButton(master=root, text='^',
-                           fg_color="black", bg_color="papaya whip",
+                           border_color = "papaya whip", fg_color="#2A0C2F", bg_color="papaya whip", hover_color = "#2B3A67", text_color="white",
                            command=lambda: press('^'),
-                           height=75, width=75, font=("Arial",28))
+                           height=75, width=75, border_width = 0.5, font=("Arial",28))
     exp_button.grid(row=2, column=1, padx=0, pady=0, sticky="nsew")
 
     sqr_root_button = CTkButton(master=root, text="√",
-                                fg_color="black", bg_color="papaya whip",
+                                border_color = "papaya whip", fg_color="#2A0C2F", bg_color="papaya whip", hover_color = "#2B3A67", text_color="white",
                                 command=inverse, 
-                                height=75, width=75, font=("Arial",28))
+                                height=75, width=75, border_width = 0.5, font=("Arial",28))
     sqr_root_button.grid(row=2, column=2, padx=0, pady=0, sticky="nsew")
 
     square_button = CTkButton(master=root, text="x^2",
-                              fg_color="black", bg_color="papaya whip",
+                              border_color = "papaya whip", fg_color="#2A0C2F", bg_color="papaya whip", hover_color = "#2B3A67", text_color="white",
                               command=square,
-                              height=75, width=75, font=("Arial",28))
+                              height=75, width=75, border_width = 0.5, font=("Arial",28))
     square_button.grid(row=3, column=0, padx=0, pady=0, sticky="nsew")
 
     open_par_button = CTkButton(master=root, text='(', 
-                                fg_color="black", bg_color="papaya whip",
+                                border_color = "papaya whip", fg_color="#2A0C2F", bg_color="papaya whip", hover_color = "#2B3A67", text_color="white",
                                 command=lambda: press('('),
-                                height=75, width=75, font=("Arial",28))
+                                height=75, width=75, border_width = 0.5, font=("Arial",28))
     open_par_button.grid(row=3, column=1, padx=0, pady=0, sticky="nsew")
 
     close_par_button = CTkButton(master=root, text=')', 
-                                fg_color="black", bg_color="papaya whip",
+                                border_color = "papaya whip", fg_color="#2A0C2F", bg_color="papaya whip", hover_color = "#2B3A67", text_color="white",
                                 command=lambda: press(')'),
-                                height=75, width=75, font=("Arial",28))
+                                height=75, width=75, border_width = 0.5, font=("Arial",28))
     close_par_button.grid(row=3, column=2, padx=0, pady=0, sticky="nsew")
 
     negate_button = CTkButton(master=root, text="+/-", 
-                               fg_color="black", bg_color="papaya whip",
+                               border_color = "papaya whip", fg_color="#2A0C2F", bg_color="papaya whip", hover_color = "#2B3A67", text_color="white",
                                command=lambda: press('-'),
-                               height=75, width=75, font=("Arial",28))
+                               height=75, width=75, border_width = 0.5, font=("Arial",28))
     negate_button.grid(row=7, column=0, padx=0, pady=0, sticky="nsew")
 
     decimal_button = CTkButton(master=root, text='.',
-                               fg_color="black", bg_color="papaya whip",
+                               border_color = "papaya whip", fg_color="#2A0C2F", bg_color="papaya whip", hover_color = "#2B3A67", text_color="white",
                                command=lambda: press('.'),
-                               height=75, width=75, font=("Arial",28))
+                               height=75, width=75, border_width = 0.5, font=("Arial",28))
     decimal_button.grid(row=7, column=2, padx=0, pady=0, sticky="nsew")
 
     equal_button = CTkButton(master=root, text='=', 
-                            fg_color="black", bg_color="papaya whip",
+                            border_color = "papaya whip", fg_color="#2A0C2F", bg_color="papaya whip", hover_color = "#2B3A67", text_color="white",
                             command=equal,
-                            height=75, width=75, font=("Arial",28))
+                            height=75, width=75, border_width = 0.5, font=("Arial",28))
     equal_button.grid(row=7, column=3, padx=0, pady=0, sticky="nsew")
 
     # =================================================
